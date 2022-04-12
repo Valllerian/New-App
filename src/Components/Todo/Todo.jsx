88a2c81574e5a793d19d6663ddadd685f0e279e5
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-11 19:43:59 -0600

    comments\steps added

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Todo = () => {
+    // this component should display a div with the task of the todo.
+    // For each todo component, there should also be a button with the "X" text that when clicked, removes the todo;
   return (
     <div>Todo</div>
   )
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const TodoList = () => {
+    // this component should render the NewToDoForm component and should render the list of Todo components;
+    // Place your state that contains all of the todos in this component;
   return (
     <div>TodoList</div>
   )
