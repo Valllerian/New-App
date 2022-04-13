574e09917b8935bda2212866d29f960cf19c5752
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-13 00:53:22 -0600

    added styling

--- src/Components/NewTodoForm/NewTodoForm.jsx
+++ src/Components/NewTodoForm/NewTodoForm.jsx
@@ -38,7 +38,7 @@ const NewTodoForm = () => {
   };
 
   return (
-    <div>
+    <div className="border border-gray-800 p-6 bg-[#6982d5] text-white">
       <form onSubmit={(e) => handleSubmit(e)}>
         <label className="mr-9">
           Task: 
@@ -47,7 +47,7 @@ const NewTodoForm = () => {
             name="task"
             value={task.value}
             onChange={(e) => handleChangeTask(e)}
-            className='border border-gray-800'
+            className='border border-gray-800 bg-[#e5f8ea] text-black'
           />
         </label>
         <label className="">
@@ -57,10 +57,10 @@ const NewTodoForm = () => {
             name="deadline"
             value={deadline.value}
             onChange={(e) => handleChangeDeadline(e)}
-            className='border border-gray-800'
+            className='border border-gray-800 bg-[#e5f8ea] text-black'
           />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" className="pl-3" />
       </form>
       <div>
         {taskArray[0]
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -5,8 +5,9 @@ const TodoList = () => {
     // this component should render the NewToDoForm component and should render the list of Todo components;
     // Place your state that contains all of the todos in this component;
   return (
-    <div className='flex h-screen justify-center items-center'><NewTodoForm/></div>
+    <div className='flex h-screen justify-center items-center bg-[#18181b]' ><NewTodoForm/></div>
   )
 }
 
 export default TodoList
+
